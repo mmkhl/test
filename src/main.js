3881311d30229719089a71ac6fa97568cef7ee83
mmkhl <pmisha1014@gmail.com>
2024-08-21 23:53:41 +0400

    feat: luxon lib added for datetime in table formatting

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import {store} from './store'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
+import { DateTime } from "luxon";
+
 
 import VueCookies from 'vue-cookies'
 
@@ -11,6 +13,7 @@ Vue.config.productionTip = false
 Vue.use(VueCookies, { expires: '7d'})
 
 Vue.prototype.$axios = axios;
+Vue.prototype.$DateTime = DateTime;
 const token = VueCookies.get('token')
 if (token) {
   Vue.prototype.$axios.defaults.headers.common['Authorization'] = `Token ${token}`
